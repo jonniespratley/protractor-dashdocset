09bee8501c485f1462bf18df5b48a7716c7ea00d
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-08-22 09:02:48 -0700

    updated

--- Protractor.docset/Contents/Resources/Documents/script.js
+++ Protractor.docset/Contents/Resources/Documents/script.js
@@ -1,45 +1,41 @@
-//Protractor DocSet
-$(document).ready(function(){
 
-	console.log('Document ready!');
+$(function () {
+	/*
+var sql = '';
+$('.protractor-api, .locators-api, .webdriver-api').children().each(function(){
+   var name = $(this).text();
+   var path = $(this).find('a').attr('href');
+   			path = path.replace(/api/g, '');
+				path = path.replace(/-/g, '');
 
+   sql += '\n';
+   sql += "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('"+name+"', 'Method', '"+ path +"');";
+});
+console.log(sql);
+*/
 
-
-	//Take all links and remove (api-) From the href
-
+	
 	//Take all .anchor elements and make the id the name
-
 	$('.anchor').each(function(){
 		var n = $(this).attr('name');
-
 				n = n.replace(/user-content-/g, '');
-
 			$(this).attr('id', n);				
-			console.log(n);
 	});
 
+	//Take all links and remove (api-) From the href
 	$('.task-list li a').each(function(){
 		var a = $(this).attr('href');
 				a = a.replace(/api/g, '');
 				a = a.replace(/-/g, '');
 
 			$(this).attr('href', a);
-
-			console.log(a);
 	});
 
-
-
-
-
-	
+	//Smooth scroll to element
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-      		console.log(target);
-
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
@@ -49,4 +45,25 @@ $(document).ready(function(){
     }
   });
 
-});
+  // hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+ 
